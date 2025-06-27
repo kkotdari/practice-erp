a3cc387ea77a3371d2c0acda2ab88954de38628a
kkotdari <kkotdari@gmail.com>
2025-06-27 17:48:15 +0900

    기본 레이아웃 짜기

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,62 @@
 <template>
   <MyFlex
-    class=""
-    style="gap:8px;"
+    class="flex-column align-items-start"
   >
     <MyFlex
       class="flex-initial justify-content-center align-items-center"
     >
       <MyFlex
-        class="flex-initial"
+        class="flex-initial flex justify-content-center align-items-center"
+        style="width: 200px;height: 200px;background-color:lightcyan;"
       >
-        <span>Top Left</span>
+        <span>
+          Logo
+        </span>
       </MyFlex>
       <MyFlex
         class="flex-initial"
+        style="width: 1800px;height: 200px;background-color: aquamarine;"
       >
-        <span>Top Left</span>
+        <span>
+          Top Right
+        </span>
       </MyFlex>
     </MyFlex>
     <MyFlex
-      class="flex-initial flex flex-column justify-content-start align-items-center"
+      class="flex-initial flex justify-content-start align-items-start"
     >
-      <MyFlex>
-        <span>
-          Top Left
-        </span>
+      <MyFlex
+        class="flex-initial flex flex-column justify-content-start gap-2"
+        style="width: 200px;height: 1600px;background-color: lightgoldenrodyellow;"
+      >
+        <button>
+            Left GNB Menu-1
+        </button>
+        <button>
+            Left GNB Menu-2
+        </button>
+        <button>
+            Left GNB Menu-3
+        </button>
+        <button>
+            Left GNB Menu-4
+        </button>
+        <button>
+            Left GNB Menu-5
+        </button>
+        <button>
+            Left GNB Menu-6
+        </button>
+        <button>
+            Left GNB Menu-7
+        </button>
       </MyFlex>
-      <MyFlex>
+      <MyFlex
+        class="flex-initial"
+        style="width: 1800px;height: 1600px;background-color: lavender;"
+      >
         <span>
-          Top Left
+          Bottom Right
         </span>
       </MyFlex>
     </MyFlex>
